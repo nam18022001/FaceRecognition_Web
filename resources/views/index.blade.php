6172651bf389aec003531a311152564dc85a5ada
Hoàng Nam <namkenly2000@gmail.com>
2021-12-23 18:16:29 +0700

    Update 23-12-2021

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,188 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <title>Điểm danh nhận diện khuôn mặt</title>
-    <style>
-        body {
-  overflow: hidden;
-  background: #E6E6FA;
-  background: linear-gradient(to right, #b24592, #f15f79)
-}
-.menu-toggler {
-  position: absolute;
-  display: block;
-  top:0;
-  bottom:0;
-  right:0;
-  left:0;
-  margin:auto;
-  width:40px;
-  height:40px;
-  z-index:2;
-  opacity:0;
-  cursor:pointer;
-}
-.menu-toggler:hover + label,
-.menu-toggler:hover + label:before,
-.menu-toggler:hover + label:after,{
-  background: white;
-}
-.menu-toggler:checked + label {
-  background: transparent;
-}
-.menu-toggler:checked + label:before,
-.menu-toggler:checked + label:after,{
-  top:0;
-  width:40px;
-  transform-origin: 50% 50%;
-}
-.menu-toggler:checked + label:before {
-  transform: rotate(45deg) translateY(-15px) translateX(-15px);
-}
-.menu-toggler:checked + label:after {
-  transform: rotate(-45deg);
-}
-.menu-toggler:checked ~ ul .menu-item { 
-  opacity: 1;
-}
-.menu-toggler:checked ~ ul .menu-item:nth-child(1) {
-  transform: rotate(0deg) translate(-110px);
-}
-.menu-toggler:checked ~ ul .menu-item:nth-child(2) {
-  transform: rotate(60deg) translateX(-110px);
-}
-.menu-toggler:checked ~ ul .menu-item:nth-child(3) {
-  transform: rotate(120deg) translateX(-110px);
-}
-.menu-toggler:checked ~ ul .menu-item:nth-child(4) {
-  transform: rotate(180deg) translateX(-110px);
-}
-.menu-toggler:checked ~ ul .menu-item:nth-child(5) {
-  transform: rotate(240deg) translateX(-110px);
-}
-.menu-toggler:checked ~ ul .menu-item:nth-child(6) {
-  transform: rotate(300deg) translateX(-110px);
-}
-.menu-toggler:checked ~ ul .menu-item a {
-  pointer-events:auto;
-}
-.menu-toggler + label {
-  width: 40px;
-  height: 5px;
-  display: block;
-  z-index: 1;
-  border-radius: 2.5px;
-  background: rgba(230, 239, 250, 0.9);
-  transition: transform 0.5s top 0.5s;
-  position: absolute;
-  display: block;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-}
-.menu-toggler + label:before,
-.menu-toggler + label:after {
-  width: 40px;
-  height: 5px;
-  display: block;
-  z-index: 1;
-  border-radius: 2.5px;
-  background: rgba(255, 255, 255, 0.7);
-  transition: transform 0.5s top 0.5s;
-  content: "";
-  position: absolute;
-  display: block;
-  left: 0;
-}
-.menu-toggler + label:before {
-  top: 10px;
-}
-.menu-toggler + label:after {
-  top: -10px;
-}
-.menu-item:nth-child(1) a {
-  transform: rotate(0deg);
-}
-.menu-item:nth-child(2) a {
-  transform: rotate(-60deg);
-}
-.menu-item:nth-child(3) a {
-  transform: rotate(-120deg);
-}
-.menu-item:nth-child(4) a {
-  transform: rotate(-180deg);
-}
-.menu-item:nth-child(5) a {
-  transform: rotate(-240deg);
-}
-.menu-item:nth-child(6) a {
-  transform: rotate(-300deg);
-}
-.menu-item {
-  position: absolute;
-  display: block;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  width: 80px;
-  height: 80px;
-  opacity: 0;
-  transition: 0.5s;
-}
-.menu-item a {
-  display: block;
-  width: inherit;
-  height: inherit;
-  line-height: 80px;
-  color: rgba(255, 255, 255, 0.7);
-  background: rgba(230, 230, 250, 0.7);
-  border-radius: 50%;
-  text-align: center;
-  text-decoration: none;
-  font-size: 40px;
-  pointer-events: none;
-  transition: 0.2s;
-}
-.menu-item a:hover {
-  box-shadow: 0 0 0 2px rgba(255, 255, 255, 0.3);
-  color: white;
-  background: rgba(255, 255, 255, 0.3);
-  font-size: 44.44px
-}
-    </style>
+    
+    <link rel="stylesheet" href="{{ asset('homepage_assets/org/jquery-circle-menu/css/style.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
 </head>
+
 <body>
-    <nav class="menu">
-        <input checked="checked" class="menu-toggler" type="checkbox">
-        <label for="menu-toggler"></label>
-        <ul>
-          <li class="menu-item" data-toggle="tooltip" data-placement="left" title="Admin">
-            <a class="far fa-cat" href="{{ url('admin') }}"></a>
-          </li>
-          <li class="menu-item" data-toggle="tooltip" data-placement="left" title="Giáo Viên">
-            <a class="fas fa-user-graduate" href="#"></a>
-          </li>
-          <li class="menu-item" data-toggle="tooltip" data-placement="left" title="Sinh Viên">
-            <a class="far fa-users-class" href="{{ url('giaovien') }}"></a>
-          </li>
-        </ul>
-      </nav>
-      <script>
-          $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
-      </script>
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<div class='selector'>
+    <ul>
+        <li >
+            <input id='1' type='checkbox'>
+            <label for='1' class="far fa-cat"> <input type="hidden" id="link1" value="{{ url('admin') }}"></label>
+        </li>
+        <li>
+            <input id='2' type='checkbox'>
+            <label for='2' class="far fa-users-class"><input type="hidden" id="link2" value="{{ url('giaovien') }}"></label>
+        </li>
+        <li>
+            <input id='3' type='checkbox'>
+            <label for='3'class="far fa-lock-alt"></label>
+        </li>
+        <li>
+            <input id='4' type='checkbox'>
+            <label for='4' class="fal fa-tools"></label>
+        </li>
+        <li>
+            <input id='5' type='checkbox'>
+            <label for='5' class="fal fa-audio-description"></label>
+        </li>
+        <li>
+            <input id='6' type='checkbox'>
+            <label for='6' class="fab fa-apple"></label>
+        </li>
+        <li>
+            <input id='7' type='checkbox'>
+            <label for='7' class="fal fa-angel"></label>
+        </li>
+        <li>
+            <input id='8' type='checkbox'>
+            <label for='8' class="fab fa-android"></label>
+        </li>
+    </ul>
+    <input type="hidden" id="linkhuaban" value="{{ asset('images/huaban.png') }}">
+    <button>Click here</button>
+</div>
+<script src="{{ asset('homepage_assets/org/jquery-circle-menu/js/jquery.min.js') }}"></script>
+<script src="{{ asset('homepage_assets/org/jquery-circle-menu/js/index.js') }}"></script>
+<script src="{{ asset('homepage_assets/org/printer-master/javascript/printer.js') }}"></script>
+<script src="{{ asset('homepage_assets/org/jquery-hb/js/snowfall.jquery.js') }}"></script>
+<script src="{{ asset('homepage_assets/org/js/christmas.js') }}"></script>
+<script src="{{ asset('homepage_assets/org/js/romantic.js') }}"></script>
 </body>
 </html>
+
